50955daf8ea5dc40fe92efada3536e81687bac9c
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-03-14 21:46:08 +0900

    ADD : AddPhotoActivity 추가

---
+++ app/src/main/java/com/example/instagram_clone/navigation/AddPhotoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.instagram_clone.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagram_clone.R
+
+class AddPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+    }
+}
