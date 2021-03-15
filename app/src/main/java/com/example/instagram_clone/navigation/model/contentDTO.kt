0750b4473bd95d3b3037238220c4b9237d6731f2
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-03-15 12:11:13 +0900

    ADD : contentDTO에 데이터 추가

--- app/src/main/java/com/example/instagram_clone/navigation/model/contentDTO.kt
+++ app/src/main/java/com/example/instagram_clone/navigation/model/contentDTO.kt
@@ -1,2 +1,16 @@
 package com.example.instagram_clone.navigation.model
 
+data class contentDTO(
+    var explain: String? = null,
+    var imageUrl: String? = null,
+    var uid: String? = null,
+    var userId: String? = null,
+    var timestamp: Long? = null,
+    var favotireCount: Int? = 0,
+    var favorites :Map<String, Boolean> = HashMap()) {
+
+    data class comments (var uid: String? = null,
+                         var userId: String? = null,
+                         var comments : String? = null,
+                         var timestamp: Long? = null)
+}
