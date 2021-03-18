ec28ec1cf532c444634d57959037e0e5183b7c6b
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-03-18 21:50:29 +0900

    ADD : XML 완성, UserFragment 중간저장

--- app/src/main/java/com/example/instagram_clone/navigation/UserFragment.kt
+++ app/src/main/java/com/example/instagram_clone/navigation/UserFragment.kt
@@ -6,12 +6,55 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram_clone.R
+import com.example.instagram_clone.navigation.model.contentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UserFragment : Fragment() {
+    var fragmentView: View? = null
+    var firestore: FirebaseFirestore? = null
+    var uid: String? = null
+    var auth: FirebaseAuth? = null
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
-        return view
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentView =
+            LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+        return fragmentView
+    }
+
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var contentDTOs: ArrayList<contentDTO> = arrayListOf()
+
+        init {
+            firestore?.collection("images")?.whereEqualTo("uid", uid)
+                ?.addSnapshotListener addSnapshotListner@{ querySnapshot, firebaseFirestoreException ->
+                    //Somtimes, This code return null of querySnapshot when it signout
+                    if (querySnapshot == null) return@addSnapshotListner
+
+                    //Get Data
+                    for (snapshot in querySnapshot.documents) {
+                        contentDTOs.add(snapshot.toObject(contentDTO::class.java)!!)
+                    }
+                }
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
     }
 }
