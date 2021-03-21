622dd950b5fe5a2dfed17479d7029acfd160ffe9
Hyeon-Bin <63246501+HYE0N1127@users.noreply.github.com>
2021-03-21 18:50:40 +0900

    중간저장

--- app/src/main/java/com/example/instagram_clone/navigation/AlarmFragment.kt
+++ app/src/main/java/com/example/instagram_clone/navigation/AlarmFragment.kt
@@ -5,13 +5,77 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram_clone.R
+import com.example.instagram_clone.navigation.model.AlarmDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.item_comment.view.*
 
 class AlarmFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_alarm, container, false)
 
         return view
+    }
+
+    inner class AlarmRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        var alarmDTOList: ArrayList<AlarmDTO> = arrayListOf()
+
+        init {
+            val uid = FirebaseAuth.getInstance().currentUser?.uid
+
+            FirebaseFirestore.getInstance().collection("alarms").whereEqualTo("destinationUid", uid)        //자신에게 온 알림만 알려줌
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    alarmDTOList.clear()
+
+                    if(querySnapshot == null) return@addSnapshotListener
+
+                    for(snapshot in querySnapshot.documents) {
+                        alarmDTOList.add(snapshot.toObject(AlarmDTO::class.java)!!)
+                    }
+                    notifyDataSetChanged()
+                }
+        }
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(p0.context).inflate(R.layout.item_comment, p0, false)
+
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return alarmDTOList.size
+        }
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            var view = p0.itemView
+
+            when (alarmDTOList[p1].kind) {
+                0 -> {
+                    val str_0 = alarmDTOList[p1].userId + getString(R.string.alarm_favorite)
+                    view.commentviewitem_textview_profile.text = str_0
+                }
+
+                1 -> {
+                    val str_0 =
+                        alarmDTOList[p1].userId + " " + getString(R.string.alarm_comment) + " of " + alarmDTOList[p1]
+                    view.commentviewitem_textview_profile.text = str_0
+                }
+
+                2 -> {
+                    val str_0 = alarmDTOList[p1].userId + " " + getString(R.string.alarm_follow)
+                    view.commentviewitem_textview_profile.text = str_0
+                }
+            }
+        }
+
     }
 }
